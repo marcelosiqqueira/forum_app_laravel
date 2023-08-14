e573f8e9b9a7cabf3d92569a64b13eacfbb0d95c
Marcelo Siqueira Filho <marcelosiqqueira@gmail.com>
2023-08-14 20:53:21 -0300

    add route for create new support

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
+Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
 Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 
 Route::get('/contato', [SiteController::class, 'contact']);
