26c7ce0c1dc2fee4789049bfbb0f69a88c30b3f9
Marcelo Siqueira Filho <marcelosiqqueira@gmail.com>
2023-10-02 22:49:42 -0300

    created Support DTO

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\DTO\CreateSupportDTO;
+use App\DTO\UpdateSupportDTO;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUpdateSupport;
 use App\Models\Support;
@@ -34,9 +36,9 @@ class SupportController extends Controller
     }
 
     public function store(Request $request, Support $support){
-        $data = $request->validated();
-        $data['status'] = 'a';
-        $support = $support->create($data);
+        $this->service->new(
+            CreateSupportDTO::makeFromRequest($request)
+        );
         return redirect()->route('supports.index');
     }
 
@@ -49,10 +51,15 @@ class SupportController extends Controller
     }
 
     public function update(StoreUpdateSupport $request, Support $support, string $id){
-        if(!$support = Support::find($id) ){
+        $support = $this->service->update(
+            UpdateSupportDTO::makeFromRequest($request)
+        );
+        return redirect()->route('supports.index');
+
+        if(!$support){
             return back();
         }
-        $support->update($request->validated());
+
         return redirect()->route('supports.index');
     }
     public function destroy(string $id){
